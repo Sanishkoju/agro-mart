e4beb22147c703e0e4ea36e3aa5a60a855a8e865
Sanishkoju <107977231+Sanishkoju@users.noreply.github.com>
2022-11-27 12:05:01 +0545

    commit

---
+++ script.js
@@ -0,0 +1,39 @@
+
+
+const toggleButton=document.getElementsByClassName('toggle-button')[0];
+console.log(toggleButton)
+const sidebar=document.getElementsByClassName('sidebar')[0]
+toggleButton.addEventListener('click',()=>{
+    sidebar.classList.toggle('active')
+})
+
+
+
+const sea=document.getElementsByClassName('searche');
+console.log(sea);
+sea.style.backgroundColor="red";
+const search=document.getElementsByClassName('search')
+console.log("search")
+searchh.addEventListener('click',(onclick)=>{
+    search.classList.toggle('active')
+
+
+
+})
+
+
+console.log(document.Element)
+console.log(document.body)
+console.log(document.head)
+console.log(document.body.childNodes)
+console.log(document.children)
+console.log(document.firstElementChild)
+console.log(document.lastChild)
+
+
+
+const ul=document.getElementsByClassName('box');
+console.log(ul);
+
+
+
